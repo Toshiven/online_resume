c4ad534e5def75d563c661dedaae54f8ba5950c6
Toshiven <kevin.abler19@gmail.com>
2024-01-15 00:11:14 +0800

    feat(main): add about me section
    feat(footer): add footer although lacks design and links
    
    using vim btw

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,55 +1,100 @@
 <script>
-
+    let benefits = [
+        {name: 'Innovative Problem Solver', description: ' I thrive tackling complex challenges and finding innovative solutions.'},
+        {name: 'Adaptability', description: 'I quickly adapt to new technologies and embrace a continuous learning mindset.'}, 
+        {name: 'Collaborative Team Player', description: ' My experiences have honed my ability to work seamlessly in collaborative environments.'},
+    ]
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section
-        id="introPage"
-        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+  <section
+    id="introPage"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+  >
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
     >
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! <br /> I'm <span class="poppins text-violet-400">Andrew Kevin</span>
+        Abler
+        <br />Aspiring
+        <span class="poppins text-violet-400">Developer</span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        My <span class="text-violet-400"> favorite tech</span> includes JavaScript
+        (React or SvelteKit), TailwindCSS, Bootstrap, and Node.js + Express.js
+      </p>
+      <button
+        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+      >
         <div
-            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
-        >
-            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! <br> I'm <span class="poppins text-violet-400">Andrew Kevin</span> Abler
-                <br />Aspiring
-                <span class="poppins text-violet-400">Developer</span>
-            </h2>
-            <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400"> favorite tech</span> includes JavaScript
-                (React or SvelteKit), TailwindCSS, Bootstrap, and Node.js + Express.js
-            </p>
-            <button
-                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
-            >
-                <div
-                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-                />
-                <h4 class="relative z-9">Get in touch &rarr;</h4>
-            </button>
-        </div>
-        <div class="relative shadow-2xl grid place-items-center ">
-            <img 
-                src={"images/profile.jpg"}
-                alt="Profile"
-                class="object-cover z-[2] max-h-[70vh] rounded-full"
-            />
-        </div>
-        <!-- <div  class="flex p-0.5 relative max-w-[700px] w-full mx-auto">
-            <div
-                class="absolute inset-0 overflow-hidden rounded-md flex items-center justify-center"
-            >
-                <div
-                    class="bg-gradient-to-r absolute inset-[-20px]  from-violet-800 to-indigo-800 specialSpin"
-                />
-            </div>
+          class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+        />
+        <h4 class="relative z-9">Get in touch &rarr;</h4>
+      </button>
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img
+        src={"images/profile.jpg"}
+        alt="Profile"
+        class="object-cover z-[2] max-h-[70vh] rounded-full"
+      />
+    </div>
+  </section>
 
-            <img
-                src={"images/zetane-engine.jpeg"}
-                alt="Zetane Engine"
-                class="w-full h-full object-cover z-[2]"
-            />
-        </div> -->
-    </section>
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-2 text-center">
+      <h6 class="text-lg sm:text-xl md:text-2xl">
+        A few of my creative endeavors.
+      </h6>
+      <h3 class="font-semibold text-3xl sm:text4xl md:text-5xl">
+        Curious to <span class="poppins text-violet-400">see</span> my work?
+      </h3>
+    </div>
 
+    <a
+      href="https://github.com/Toshiven"
+      target="_blank"
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+    >
+      <i class="fa-brands fa-github"></i>
+      <p>Click here!</p>
+    </a>
+
+    <div></div>
+  </section>
+
+  <section
+    id="about"
+    class="py-20 pt-10 lg:pt16 lg:py32 flex flex-col gap-16 sm:gap20 md:24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A bit <span class="poppins text-violet-400">about</span> me.
+      </h3>
+              <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            I am . . .
+        </p>
+                <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+    </div>
+  </section>
 </main>
